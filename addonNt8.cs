dd0aa2cffee787b1a26fdff8e4f07175696c958d
teamchartvn <146596976+teamchartvn@users.noreply.github.com>
2024-05-16 08:13:55 +0700

    Update addonNt8.cs

--- addonNt8.cs
+++ addonNt8.cs
@@ -10,8 +10,6 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Xml.Serialization;
-using System.IO.MemoryMappedFiles;
-
 using NinjaTrader.Cbi;
 using NinjaTrader.Gui;
 using NinjaTrader.Gui.Chart;
@@ -20,80 +18,129 @@ using NinjaTrader.Gui.Tools;
 using NinjaTrader.Data;
 using NinjaTrader.NinjaScript;
 using NinjaTrader.Core.FloatingPoint;
-using NinjaTrader.Gui.Tools;
+using NinjaTrader.NinjaScript.DrawingTools;
 #endregion
 
-//This namespace holds Add ons in this folder and is required. Do not change it. 
-namespace NinjaTrader.NinjaScript.AddOns
+//This namespace holds Indicators in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Indicators
 {
-	public class SHAREMEM : NinjaTrader.NinjaScript.AddOnBase
+	public class Acanhbaokietvol : Indicator
 	{
-		
-		/// <summary>
-///   File .Net
-/// </summary>
-	public string memoryMappedFileName = "Buf";
-    private MemoryMappedViewAccessor doc_ghi;
-	private  const int MemorySize =128;
-        // Hàm đọc ghi chia sẻ bộ nhớ theo .Net
-        public  SHAREMEM(){
-		
-		using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("Buf"))
-        using (MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor())
-        {
-          // Đọc dữ liệu từ vùng nhớ chia sẻ
-          doc_ghi = accessor;
-        }
-		
-	}
-	
-		
-public void  DocGhi(double cao , double thap, double dong , double mo){
-	  // đọc dữ liệu byte 127
-	  byte flag = doc_ghi.ReadByte(MemorySize - 1);
-	  byte[] data = new byte[MemorySize - 1];
-      doc_ghi.ReadArray<byte>(0, data, 0, data.Length);
-	  ChuyenDoiByte(ref data,cao);
-	  ChuyenDoiByte(ref data,thap);
-	  ChuyenDoiByte(ref data,dong);
-	  ChuyenDoiByte(ref data,mo);
-	  
-	
-	
-}
-
-
-
-
-
-private void ChuyenDoiByte(ref byte [] data , double gia){
-	         
-	          byte[] bytes = BitConverter.GetBytes(gia);
-
-              // Sao chép mảng byte vào mảng data
-              for (int i = 0; i < bytes.Length; i++)
-              {
-               data[currentIndex + i] = bytes[i];
-              }
-}
-
-
-
-
-
-
-		
 		protected override void OnStateChange()
 		{
 			if (State == State.SetDefaults)
 			{
-				Description									= @"Enter the description for your new custom Add on here.";
-				Name										= "MyCustomAddOn2";
+				Description									= @"Enter the description for your new custom Indicator here.";
+				Name										= "2-Cảnh báo kiệt vol";
+				Calculate									= Calculate.OnBarClose;
+				IsOverlay									= false;
+				DisplayInDataBox							= true;
+				DrawOnPricePanel							= true;
+				DrawHorizontalGridLines						= true;
+				DrawVerticalGridLines						= true;
+				PaintPriceMarkers							= true;
+				ScaleJustification							= NinjaTrader.Gui.Chart.ScaleJustification.Right;
+				//Disable this property if your indicator requires custom values that cumulate with each new market data event. 
+				//See Help Guide for additional information.
+				IsSuspendedWhileInactive					= true;
 			}
 			else if (State == State.Configure)
 			{
-				
 			}
+		}
+
+		protected override void OnBarUpdate()
+		{
+			Print(CurrentBar);
+	if(BarsInProgress == 0 && CurrentBar > 5)
+			{
+				
+				if(Bars.GetVolume(CurrentBar) < 0.4*Bars.GetVolume(CurrentBar-1) ){
+					Print("Thap hon bat thuong dieu kien 1");
+					float trungbinh = ( Bars.GetVolume(CurrentBar-1) + Bars.GetVolume(CurrentBar-2)+Bars.GetVolume(CurrentBar-3))/3 ;
+                   Print("trung binh 3 nến : ");
+					if( trungbinh > Bars.GetVolume(CurrentBar)){
+						 Print ("thoa man dieu kien trung binh ");
+					}
+					
+					////////////////////////////////////////
+					/// 
+					
+                                  if (Close[0] > Open[0]){
+                                  Draw.ArrowUp(this, "Mua", true, 0, Low[0] - TickSize, Brushes.Red);
+								  }else{
+						
+								
+                                     Draw.ArrowDown(this, "Bán", true, 0, High[0] - TickSize, Brushes.Red);
+								
+								  }
+					
+					
+					
+					
+				}
+			}
+			else{
+				Print("xit !");
+			}
+			//Add your custom indicator logic here.
 		}
 	}
 }
+
+#region NinjaScript generated code. Neither change nor remove.
+
+namespace NinjaTrader.NinjaScript.Indicators
+{
+	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
+	{
+		private Acanhbaokietvol[] cacheAcanhbaokietvol;
+		public Acanhbaokietvol Acanhbaokietvol()
+		{
+			return Acanhbaokietvol(Input);
+		}
+
+		public Acanhbaokietvol Acanhbaokietvol(ISeries<double> input)
+		{
+			if (cacheAcanhbaokietvol != null)
+				for (int idx = 0; idx < cacheAcanhbaokietvol.Length; idx++)
+					if (cacheAcanhbaokietvol[idx] != null &&  cacheAcanhbaokietvol[idx].EqualsInput(input))
+						return cacheAcanhbaokietvol[idx];
+			return CacheIndicator<Acanhbaokietvol>(new Acanhbaokietvol(), input, ref cacheAcanhbaokietvol);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
+{
+	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
+	{
+		public Indicators.Acanhbaokietvol Acanhbaokietvol()
+		{
+			return indicator.Acanhbaokietvol(Input);
+		}
+
+		public Indicators.Acanhbaokietvol Acanhbaokietvol(ISeries<double> input )
+		{
+			return indicator.Acanhbaokietvol(input);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
+	{
+		public Indicators.Acanhbaokietvol Acanhbaokietvol()
+		{
+			return indicator.Acanhbaokietvol(Input);
+		}
+
+		public Indicators.Acanhbaokietvol Acanhbaokietvol(ISeries<double> input )
+		{
+			return indicator.Acanhbaokietvol(input);
+		}
+	}
+}
+
+#endregion
